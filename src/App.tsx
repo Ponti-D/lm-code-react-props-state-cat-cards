cc1b1efa69f3552ce5d935799a92a1a72470edbe
Ponti-D <spaediaz@gmail.com>
2023-01-30 15:25:51 +0000

    Implemented activity 7 - header

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import Cat from "./data/cat";
 import CatCard from "./components/cat_card";
 
+
 function App(): JSX.Element {
   // JavaScript/TypeScript code can be inserted here!
   const [cats, setCats] = useState<Array<Cat>>([
@@ -81,25 +82,33 @@ function App(): JSX.Element {
       favFoods: ["milk"],
       birthYear: 2021,
     },
+    {
+      name: "Captain Catface",
+      species: "Sea Cattain",
+      favFoods: ["fish, rum"],
+      birthYear: 2016,
+    }
   ]);
   console.log("Our pretties 😻: ", cats);
+  const catCount = cats.length;
+  console.log(catCount);
 
   return (
     <>
       <Navbar />
-      <Header />
-
+      <Header catCount={catCount}/>
+      
       <main>
         <div className="cards__wrapper">
-        {cats.map((cat, index) => (
-						<CatCard
-							name={cat.name}
-							species={cat.species}
-							favFoods={cat.favFoods}
-							birthYear={cat.birthYear}
-							catIndex={index}
-						/>
-					))}
+          {cats.map((cat, index) => (
+            <CatCard
+              name={cat.name}
+              species={cat.species}
+              favFoods={cat.favFoods}
+              birthYear={cat.birthYear}
+              catIndex={index}
+            />
+          ))}
         </div>
       </main>
 
